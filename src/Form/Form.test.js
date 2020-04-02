d498979d8e31138fc3f8a89689f3a80d35df7fe3
Lane Hunter <lanehunter.96@gmail.com>
2020-04-02 11:29:03 -0600

    Add passing test for rendering form component

---
+++ src/Form/Form.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Form from './Form';
+import "@testing-library/jest-dom";
+import { render, fireEvent } from "@testing-library/react";
+
+describe('Form', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<Form />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should render all inputs on the page', () => {
+    const { getByPlaceholderText } = render(<Form />);
+    const namePlaceholder = getByPlaceholderText('Name')
+    const datePlaceholder = getByPlaceholderText('Date (m/dd)')
+    const timePlaceholder = getByPlaceholderText('Time')
+    const numPlaceholder = getByPlaceholderText('Number of guests')
+
+    expect(namePlaceholder).toBeInTheDocument()
+    expect(datePlaceholder).toBeInTheDocument()
+    expect(timePlaceholder).toBeInTheDocument()
+    expect(numPlaceholder).toBeInTheDocument()
+  })
+
+  it('should update state when inputs are filled out', () => {
+
+  })
+
+})
