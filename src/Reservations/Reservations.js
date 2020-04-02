c8dd300fdb90704890cb35b72c77bf32459acb9f
Lane Hunter <lanehunter.96@gmail.com>
2020-04-02 09:16:13 -0600

    Add reservations directory and initial setup

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Reservations extends React.Component {
+  contructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  
+  render () {
+
+  }
+}
+
+export default Reservations;
