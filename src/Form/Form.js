cf5eff6f2b3c3d746dab6805c805b9158a1fa558
Lane Hunter <lanehunter.96@gmail.com>
2020-04-02 10:48:42 -0600

    Add state-changing form inputs

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './Form.css'
 
 class Form extends React.Component {
   constructor() {
@@ -11,10 +12,36 @@ class Form extends React.Component {
       number: 1
     }
   }
+
+  updateState(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
   render () {
     return (
-      <input
-        placeholder="Name"></input>
+      <form>
+        <input
+          placeholder="Name"
+          name="name"
+          onChange={(event) => this.updateState(event)}
+          ></input>
+        <input
+          placeholder="Date (m/dd)"
+          name="date"
+          onChange={(event) => this.updateState(event)}
+          ></input>
+        <input
+          placeholder="Time"
+          name="time"
+          onChange={(event) => this.updateState(event)}
+          ></input>
+        <input
+          placeholder="Number of guests"
+          name="number"
+          onChange={(event) => this.updateState(event)}
+          ></input>
+        <button id="submit">Make Reservation</button>
+      </form>
     )
   }
 }
