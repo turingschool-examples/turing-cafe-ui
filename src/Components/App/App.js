f8a98ef70b27a1cc90eb0312a58bb156d806a9d4
pcmueller <pmuellerleile@gmail.com>
2021-05-26 10:13:05 -0600

    Complete Reservation data display and styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import Reservation from '../Reservation/Reservation';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state={
+      allReservations: [],
+    }
+  }
+
+  getAllReservations = () => {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ allReservations: data })
+      });
+  }
+
+  componentDidMount = () => {
+    this.getAllReservations();
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -10,7 +31,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          <Reservation allReservations={this.state.allReservations} />
         </div>
       </div>
     )
