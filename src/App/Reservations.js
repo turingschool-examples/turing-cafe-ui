ebae464b578b2bdc074d5902359438bf3133bb6c
Em Orendorff <em.orendorff@gmail.com>
2021-07-21 10:57:47 -0500

    Add reservation component to map over Card component

---
+++ src/App/Reservations.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Card from './Card.js'
+import './Reservations.css'
+
+const Reservations = ( { resy }) => {
+    const allCards = resy.map(reservation => {
+        return ( 
+            <Card
+                id={reservation.id}
+                name={reservation.name}
+                date={reservation.date}
+                time={reservation.time}
+                number={reservation.number}
+            />
+        )
+    })
+
+    return (
+        <div className='card-holder'>
+            {allCards}
+        </div>
+    )
+}
+
+export default Reservations
