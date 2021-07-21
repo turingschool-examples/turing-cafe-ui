ea8f6c49dc40f98eeee248d6fe101978d9fa2b7d
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-21 09:55:40 -0600

    start buildout of reservation component

---
+++ src/Reservations/Reservations.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Reservations = ({reservations}) => {
+
+    let allReservations = reservations.map(resy => {
+        <Card
+        
+        />
+    })
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+
+
+export default Reservations;
