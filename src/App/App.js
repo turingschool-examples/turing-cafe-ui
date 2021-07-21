849322967adb8cb49a68797f6005785d8c68708c
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-07-21 09:35:03 -0600

    add data to state

--- src/App/App.js
+++ src/App/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         {"id":6,"name":"Eric","date":"5/30","time":"6:00","number":8},
         {"id":7,"name":"Robbie","date":"6/5","time":"5:30","number":2},
         {"id":8,"name":"Travis","date":"6/8","time":"7:00","number":12},
-        {"id":9,"name":"Brittany","date":"9/9","time":"7:30","number":3}
+        {"id":9,"name":"Brittany","date":"9/9","time":"7:30","number":3},
       ]
     }
   }
