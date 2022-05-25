16b91a9153f8aea46ef45953b82963f2efbac94e
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-25 09:45:49 -0600

    Set up reservation card basic strucutre

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './ReservationCard.css'
 
-const ReservationCard = () => {
+const ReservationCard = ({ id, name, date, time, number }) => {
 
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>{number}</p>
+    </div>
+  )
 }
 
 export default ReservationCard;
