867ca8779fff361df0aba9d9c6d6b4445bd6677d
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-25 09:59:19 -0600

    Update card styling

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -4,11 +4,11 @@ import './ReservationCard.css'
 const ReservationCard = ({ id, name, date, time, number }) => {
 
   return (
-    <div>
+    <div className="resy-card">
       <p>{name}</p>
       <p>{date}</p>
-      <p>{time}</p>
-      <p>{number}</p>
+      <p>{time} pm</p>
+      <p>Number of guests: {number}</p>
     </div>
   )
 }
