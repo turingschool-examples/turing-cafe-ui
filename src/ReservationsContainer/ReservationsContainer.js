4ca138041481e61b0d7621d3a8235499f50eacfd
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-25 09:39:58 -0600

    Set up basic reservations container rendering a string for now

---
+++ src/ReservationsContainer/ReservationsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReservationCard from '../ReservationCard/ReservationCard.js'
+import './ReservationsContainer.css'
+
+const ReservationsContainer = ({ reservations  }) => {
+  return (
+    <h1>hello</h1>
+  )
+}
+
+export default ReservationsContainer;
