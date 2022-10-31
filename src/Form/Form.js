6c27e9268269412924648dc2a98a11d4b7baa69e
Emily Miles <ms.emily.j.miles@gmail.com>
2022-10-31 13:03:04 -0400

    Add functionality and stying to form components

---
+++ src/Form/Form.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import './Form.css';
+
+class Form extends Component {
+  constructor () {
+    super();
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      number: '',
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  submitReservation = event => {
+    event.preventDefault();
+    const newResy = {
+      id: Date.now(),
+      ...this.state
+    };
+    this.props.addReservation(newResy);
+    this.clearInputs();
+  };
+
+  clearInputs = () => {
+    this.setState({ name: '', date: '', time: '', number: 0 });
+  };
+
+  render() {
+    return (
+      <form className="form-section">
+        <input
+          type='text'
+          placeholder="name"
+          name='name'
+          value={ this.state.name }
+          onChange={ event => this.handleChange(event) }
+        />
+        <input
+          type='text'
+          placeholder="date"
+          name='date'
+          value={ this.state.date }
+          onChange={ event => this.handleChange(event) }
+        />
+        <input
+          type='text'
+          placeholder="time"
+          name='time'
+          value={ this.state.time }
+          onChange={ event => this.handleChange(event) }
+        />
+        <input
+          type='number'
+          placeholder="number of guests"
+          name='number'
+          value={ this.state.number }
+          onChange={ event => this.handleChange(event) }
+        />
+        <button onClick={ event => this.submitReservation(event) }>Make Reservation</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
