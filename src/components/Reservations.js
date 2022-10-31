e9335d5d956fbf9d5f5609754875345c38a62128
Cole Anthony <cole.edwin.anthony@gmail.com>
2022-10-31 09:58:23 -0600

    iterate through resys to be able to display all

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Resy from './Resy'
 
 const Reservations = ({reservations}) => {
-const resyCards = reservations.map(resy => {
+const resys = reservations.map(resy => {
   return (
-   
+   <Resy
+    id={resy.id}
+    name={resy.name}
+    date={resy.date}
+    time={resy.time}
+    number={resy.number}
+    key={resy.id}
+   />
   )
 })
+
+return (
+  <div className='resy-container'>
+    {resys}
+  </div>
+)
 }
 
 
---
+++ src/components/Resy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Resy = ({id, name, date, time, number}) => {
+  
+  return (
+    <div>
+      <h1>{name}</h1>
+      
+      
+    </div>
+  )
+}
+
+export default Resy
