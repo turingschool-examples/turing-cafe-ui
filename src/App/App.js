4c4efd7f4d3731d21b6ee89c853fead7b1e2144a
Emily Miles <ms.emily.j.miles@gmail.com>
2022-10-31 13:02:01 -0400

    Add functionality and stying to app components

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import Reservations from '../Reservations/Reservations';
+import Form from '../Form/Form';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      reservations: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(response => response.json())
+      .then(data => this.setState({ reservations: data }));
+  }
+
+  addReservation = newResy => {
+    this.setState({ reservations: [...this.state.reservations, newResy] });
+  };
+
+  cancelReservation = id => {
+    const filteredResys = this.state.reservations.filter(resy => resy.id !== id);
+
+    this.setState({ reservations: filteredResys });
+  };
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form addReservation={ this.addReservation } />
         </div>
         <div className='resy-container'>
-          
+          <Reservations reservations={ this.state.reservations } cancelReservation={ this.cancelReservation } />
         </div>
       </div>
-    )
+    );
   }
 }
 
