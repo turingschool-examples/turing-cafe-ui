320a4fa6957f22a2dc4a18348ea50ef860cbb97f
Blanche Haddad <blancherachel@gmail.com>
2023-02-21 09:42:24 -0700

    feat: Display props onto DOM

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({ id, name, date, time, number }) {
   return (
     <div>
-      
+      <h2 className="name">{name}</h2>
+      <p className="date">{date}</p>
+      <p className="time">{time}</p>
+      <p className="number">Number of guests: {number}</p>
+      <button classname="cancel-button">Cancel</button>
     </div>
   )
 }
