73c11552fd6379cc2d25068a8150f8551275d250
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-02-21 12:26:49 -0500

    Add style to Card component body

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './Cards.css'
 
-const Cards = () => {
+const Cards = ({date,id, key,name,number,time}) => {
   return(
-    <section>
-      
+    <section className="card-body">
+      <h2> {name} </h2>
+      <h3> {date} </h3>
+      <h3>{time} pm </h3>
+      <h3> Number of guest: {number} </h3>
+      <button className="card-button"> Cancel </button>
     </section>
   )
 
 }
 export default Cards
+
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -3,14 +3,23 @@ import './Display.css';
 import Cards from "../Cards/Cards";
 
 const Display = ({reservations}) => {
-  const reservedReservations = reservations.map((reservedSlot) => {
-    console.log(reservedSlot)
+  const reservedReservations = reservations.map((reservedSlot) => 
    
-  })
+   <Cards
+    date={reservedSlot.date}
+    id={reservedSlot.id}
+    key={reservedSlot.id}
+    name={reservedSlot.name}
+    number={reservedSlot.number}
+    time={reservedSlot.time}
+   />
+   )
 
   return(
-    <section>
+    <section className="display-area">
+      {reservedReservations}
     </section>
   )
 }
 export default Display
+
