3db1d1ac1d3b1d5c458551646069d1b16b005b1f
Blanche Haddad <blancherachel@gmail.com>
2023-02-21 10:12:29 -0700

    feat: Add Form component

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import fetchReservations from '../apiCalls';
 import Reservations from "../Reservations/Reservations";
+import Form from '../Form/Form';
 
 class App extends Component {
   constructor() {
@@ -9,6 +10,10 @@ class App extends Component {
     this.state = {
       reservations: []
     }
+  }
+
+  addReservation = (newRes) => {
+    this.setState({reservations: [...this.state.reservations, newRes]})
   }
 
   componentDidMount() {
@@ -23,7 +28,9 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form 
+          addReservation={this.addReservation}
+          />
         </div>
         <div className='resy-container'>
           <Reservations 
