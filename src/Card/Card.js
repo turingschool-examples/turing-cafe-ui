33c818da93edb8249e30f319719b8bc25a66687e
Blanche Haddad <blancherachel@gmail.com>
2023-02-21 09:50:31 -0700

    feat: Change typo

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Card.css";
 
 export default function Card({ id, name, date, time, number }) {
   return (
-    <div>
+    <div className="card">
       <h2 className="name">{name}</h2>
       <p className="date">{date}</p>
       <p className="time">{time}</p>
       <p className="number">Number of guests: {number}</p>
-      <button classname="cancel-button">Cancel</button>
+      <button className="cancel-button">Cancel</button>
     </div>
   )
 }
