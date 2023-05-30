63fcfb4f6bcc542ff4de601c2f6946dedb6e60d7
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-05-30 08:35:20 -0700

    Adds api data

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: [],
+      error: ""
+    }
+  }
+
+  componentDidMount =() => {
+    fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => {
+      if(!response.ok) {
+        throw new Error(`${response.status}`)
+      } else {
+        return response.json()
+      }
+    })
+    .then(data =>this.setState({reservations: data}))
+    .catch(err => this.setState({error: err.message}))
+    } 
+
+
   render() {
     return (
       <div className="App">
