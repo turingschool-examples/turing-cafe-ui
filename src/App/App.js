538947065f1957c7f53494c90de47d3d9f1cad59
Scott Ertmer <scottertmer@gmail.com>
2023-07-27 08:49:23 -0600

    Update App to functional component

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
 import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
-        </div>
-        <div className='resy-container'>
-          
-        </div>
+function App() {
+  return (
+    <div className="App">
+      <h1 className='app-title'>Turing Cafe Reservations</h1>
+      <div className='resy-form'>
       </div>
-    )
-  }
+      <div className='resy-container'>
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default App; 
