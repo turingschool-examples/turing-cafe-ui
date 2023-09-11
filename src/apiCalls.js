dba9959f2c329377336a06fd03bb9d1f140ce594
Jen Ziel <jenziel400@gmail.com>
2023-09-11 11:30:06 -0400

    Add fetch call to apiCalls.js.  Import fetch call to App.js

---
+++ src/apiCalls.js
@@ -0,0 +1,9 @@
+export const getReservationData = () => {
+    return fetch("http://localhost:3001/api/v1/reservations")
+    .then((response) => {
+        if(!response.ok){
+            throw new Error("There was an error retrieving your data.")
+        }
+        return response.json()
+    })
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { getReservationData } from '../../apiCalls';
 
 function App() {
   const [reservations, setReservations] = useState([]);
