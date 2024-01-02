1dd80448a74b4a3ea55fc2eaf38ebcdf8bc0e1df
Zen McMillan <zenmcmillan@gmail.com>
2024-01-02 12:01:58 -0600

    Add testData file and before each to form test file

---
+++ cypress/fixtures/testData.js
@@ -0,0 +1,65 @@
+ [
+{
+"id": 1,
+"name": "Christie",
+"date": "12/29",
+"time": "7:00",
+"number": 12
+},
+{
+"id": 2,
+"name": "Leta",
+"date": "4/5",
+"time": "7:00",
+"number": 2
+},
+{
+"id": 3,
+"name": "Pam",
+"date": "1/21",
+"time": "6:00",
+"number": 4
+},
+{
+"id": 4,
+"name": "Khalid",
+"date": "5/9",
+"time": "7:30",
+"number": 7
+},
+{
+"id": 5,
+"name": "Will",
+"date": "5/15",
+"time": "6:30",
+"number": 2
+},
+{
+"id": 6,
+"name": "Eric",
+"date": "5/30",
+"time": "6:00",
+"number": 8
+},
+{
+"id": 7,
+"name": "Robbie",
+"date": "6/5",
+"time": "5:30",
+"number": 2
+},
+{
+"id": 8,
+"name": "Travis",
+"date": "6/8",
+"time": "7:00",
+"number": 12
+},
+{
+"id": 9,
+"name": "Brittany",
+"date": "9/9",
+"time": "7:30",
+"number": 3
+}
+]
